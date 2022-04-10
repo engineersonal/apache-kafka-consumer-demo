2f15382b35b3771b5abbbcea875cf6eb32907733
Sharma <sonal.sharma04@sap.com>
2022-04-10 22:35:15 +0530

    Initial commit

---
+++ src/test/java/com/sonal/kafka/ApacheKafkaConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonal.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaConsumerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
